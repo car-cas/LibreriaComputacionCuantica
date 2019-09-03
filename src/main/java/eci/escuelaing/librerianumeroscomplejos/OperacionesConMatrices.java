f560460a3a8707a37dcfb7b58b3c2ecb733bd02c
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2019-09-02 22:19:00 -0500

    ultimos metodos y test

--- src/main/java/eci/escuelaing/librerianumeroscomplejos/OperacionesConMatrices.java
+++ src/main/java/eci/escuelaing/librerianumeroscomplejos/OperacionesConMatrices.java
@@ -19,10 +19,36 @@ public class OperacionesConMatrices {
      
     public OperacionesConMatrices(){
         operacion = new Operaciones();
-    }   
+    }  
+    
     public Complejo[][] getMatriz(){
         return matriz;
     }
+    
+    public Integer tamanio() {
+        return matriz.length;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        OperacionesConMatrices mc = (OperacionesConMatrices) obj;
+        if (this.tamanio() != mc.tamanio()) {
+            return false;
+        }
+        boolean flag = false;
+        for (int i = 0; i < matriz.length; i++) {
+            Complejo[] numeroComplejos = matriz[i];
+
+        }
+        
+        return true;
+    }
+    
     /**
      * 
      * Metodo adicionDeMatrices, calcula la suma de dos matrices 
@@ -123,8 +149,30 @@ public class OperacionesConMatrices {
         respuesta = matrizConjugada(respuesta);
         return respuesta;
     }
-    
-    
+    /**
+     * Metodo accionMatrizSobreVector, calcula la accion de una matriz sobre
+     * un vector 
+     * @param m1 Complejo[][]
+     * @param v Complejo[]
+     * @return Complejo[]
+     * @throws Exception 
+     */
+    public  Complejo[] accionMatrizSobreVector(Complejo[][] m1,Complejo[] v) throws Exception{
+        if (m1[0].length != v.length) {
+            throw new Exception("la longitud de las filas de la matriz es diferente a la longitud el vector");
+	} else {
+            Complejo[] r = new Complejo[v.length];  
+            Complejo s = new Complejo(0,0);
+            for (int i = 0; i < v.length; i++) {
+                for (int j = 0; j < m1[0].length; j++) {
+                    s = operacion.suma(s, operacion.producto(m1[i][j], v[j]));
+		}
+		r[i] = s;
+		s = new Complejo(0,0);
+            }
+            return r;
+	}
+    }
     /**
      * Metodo normaMatrices, calcula la norma de una matriz de numeros reales.
      * 
@@ -192,6 +240,90 @@ public class OperacionesConMatrices {
             trace = trace + r[i][i];
 	}
 	return Math.sqrt(trace);
+    } 
+    /**
+     * Metodo matrizMultiplicacion, multiplica dos matrices de complejos
+     * @param m1 Complejo[][]
+     * @param m2 Complejo[][]
+     * @return Complejo[][]
+     * @throws Exception 
+     */
+    public Complejo[][] matrizMultiplicacion(Complejo[][] m1,Complejo[][] m2) throws Exception {
+        if (m1.length != m2[0].length) {
+            throw new Exception("la matriz no es cuadrada");
+        } else {
+            Complejo[][] r = new Complejo[m1.length][m1[0].length];
+            Complejo s = new Complejo(0, 0);
+            for (int m = 0; m < m1.length; m++) {
+                for (int n = 0; n < m1[0].length; n++) {
+                    for (int i = 0; i < m1.length; i++) {
+                        Complejo temp = operacion.producto(m1[m][i], m2[i][n]);
+                        s = operacion.suma(s, temp);
+                    }
+                    r[m][n] = s;
+                    s = new Complejo(0, 0);
+                }
+            }
+            return r;
+        }
+    } 
+    /**
+     * Metodo matrizUnitaria, verifica que la matriz sea unitaria
+     * @param m1 Complejo [][]
+     * @return boolean 
+     * @throws Exception 
+     */
+    public boolean matrizUnitaria(Complejo[][] m1) throws Exception {
+        if (m1.length != m1[0].length) {
+            throw new Exception("la matriz no es cuadrada");
+        } else {
+            boolean flag;
+            flag = this.matrizMultiplicacion(m1, this.matrizAdjunta(m1)).equals(this.matrizMultiplicacion(this.matrizAdjunta(m1), m1));
+            return flag;
+        }
     }
-       
-}
+    /**
+     * Metodo matrizHermitiana, verifica que la matriz sea hermitiana
+     * @param m1 Complejo [][]
+     * @return boolean 
+     * @throws Exception 
+     */
+    public  boolean matrizHermitiana(Complejo[][] m1) throws Exception {
+        if (m1.length != m1[0].length) {
+            throw new Exception("la matriz no es cuadrada");
+	} else {
+            return m1.equals(matrizAdjunta(m1));
+	}
+    }
+    /**
+     * Metodo productoTensor, calcula el producto tensor de dos matrices
+     * @param m1 Complejo[][]
+     * @param m2 Complejo[][]
+     * @return Complejo[][]
+     */
+    public  Complejo[][] productoTensor(Complejo[][] m1, Complejo[][] m2) {
+        Complejo[][] r = new Complejo[m1.length * m2.length][ m1[0].length * m2[0].length];
+	Complejo[][] sp;
+	int m = 0;
+	int n = 0;
+	for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[0].length; j++) {
+                sp = this.multiplicacionEscalarPorMatriz(m2,m1[i][j]);
+		for(int k = 0; k < sp.length; k++) {
+                    for(int l = 0; l < sp[0].length; l++) {
+                        r[m][n] = sp[k][l];
+			n++;
+                    }
+                    m++;
+                    n = j * m2[0].length;
+		}
+                m = i * m2.length;
+		n = (j + 1) * m2[0].length;
+            }
+            m = (i + 1) * m2.length;
+            n = 0;
+        }
+	return r;
+    }
+}   
+
--- src/test/java/OperacionesConMatricesTest.java
+++ src/test/java/OperacionesConMatricesTest.java
@@ -17,8 +17,117 @@ import org.junit.Test;
  */
 public class OperacionesConMatricesTest {
     
+  
     @Test
-    public void matrixNormTest() throws Exception {
+    public void adicionMatrizTest(){  
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][] listaDos = new Complejo[2][2];
+        Complejo[][] esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,0);
+        lista[0][1] =new Complejo(0,1);
+        lista[1][0] = new Complejo(1,0);
+        lista[1][1] = new Complejo(0,1);
+        listaDos[0][0] = new Complejo(0,1);
+        listaDos[0][1] = new Complejo(1,0);
+        listaDos[1][0]= new Complejo(0,1);
+        listaDos[1][1]= new Complejo(1,0);
+        esperado[0][0] = new Complejo(1,1);
+        esperado[0][1] = new Complejo(1,1);
+        esperado[1][0] = new Complejo(1,1);
+        esperado[1][1] = new Complejo(1,1);
+        Complejo[][] respuesta = operacion.adicionDeMatrices(lista, listaDos) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+    public void inversaMatrizTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,1);
+        lista[0][1] =new Complejo(1,1);
+        lista[1][0] = new Complejo(1,1);
+        lista[1][1] = new Complejo(1,1);
+        esperado[0][0] = new Complejo(-1,-1);
+        esperado[0][1] = new Complejo(-1,-1);
+        esperado[1][0] = new Complejo(-1,-1);
+        esperado[1][1] = new Complejo(-1,-1);
+        Complejo[][] respuesta = operacion.inversaDeMatriz(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+    public void escalarMatrizTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,1);
+        lista[0][1] =new Complejo(1,1);
+        lista[1][0] = new Complejo(1,1);
+        lista[1][1] = new Complejo(1,1);
+        esperado[0][0] = new Complejo(0,4);
+        esperado[0][1] = new Complejo(0,4);
+        esperado[1][0] = new Complejo(0,4);
+        esperado[1][1] = new Complejo(0,4);
+        Complejo nc= new Complejo(2,2);
+        Complejo[][] respuesta = operacion.multiplicacionEscalarPorMatriz(lista, nc) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+    public void matrizTranspuestaTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,0);
+        lista[0][1] =new Complejo(1,0);
+        lista[1][0] = new Complejo(0,1);
+        lista[1][1] = new Complejo(1,0);
+        esperado[0][0] = new Complejo(1,0);
+        esperado[0][1] = new Complejo(0,1);
+        esperado[1][0] = new Complejo(1,0);
+        esperado[1][1] = new Complejo(1,0);
+        Complejo[][] respuesta = operacion.matrizTranspuesta(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+     public void matrizConjugadaTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,1);
+        lista[0][1] =new Complejo(1,1);
+        lista[1][0] = new Complejo(1,1);
+        lista[1][1] = new Complejo(1,1);
+        esperado[0][0] = new Complejo(-1,-1);
+        esperado[0][1] = new Complejo(-1,-1);
+        esperado[1][0] = new Complejo(-1,-1);
+        esperado[1][1] = new Complejo(-1,-1);
+        Complejo[][] respuesta = operacion.matrizConjugada(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+     
+    @Test
+    public void matrizAdjuntaTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,0);
+        lista[0][1] =new Complejo(1,0);
+        lista[1][0] = new Complejo(0,1);
+        lista[1][1] = new Complejo(1,0);
+        esperado[0][0] = new Complejo(1,0);
+        esperado[0][1] = new Complejo(0,-1);
+        esperado[1][0] = new Complejo(1,0);
+        esperado[1][1] = new Complejo(1,0);
+        Complejo[][] respuesta = operacion.matrizAdjunta(lista) ;
+        assertEquals(respuesta,esperado);
+    } 
+    
+    @Test
+    public void matrizNormaTest() throws Exception {
         OperacionesConMatrices operacion = new OperacionesConMatrices();
         double[][] m = new double[2][2];
 	m[0][0] = 3;
@@ -30,21 +139,70 @@ public class OperacionesConMatricesTest {
 	assertEquals(expected, actual, 0.7);
     }
 
-	@Test
-	public void matrixDistanceTest() throws Exception {
-            OperacionesConMatrices operacion = new OperacionesConMatrices();
-            double[][] m1 = new double[2][2];
-            m1[0][0] = 3;
-            m1[0][1] = 5;
-            m1[1][0] = 2;
-            m1[1][1] = 3;
-            double[][] m2 = new double[2][2];
-            m2[0][0] = 1;
-            m2[0][1] = 6;
-            m2[1][0] = 2;
-            m2[1][1] = 4;
-            double expected = 2.45;
-            double actual = operacion.distanciaEntreMatrices(m1, m2);
-            assertEquals(expected, actual, 0.1);
-	}
+    @Test
+    public void matrizDistanciaTest() throws Exception {
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        double[][] m1 = new double[2][2];
+        m1[0][0] = 3;
+        m1[0][1] = 5;
+        m1[1][0] = 2;
+        m1[1][1] = 3;
+        double[][] m2 = new double[2][2];
+        m2[0][0] = 1;
+        m2[0][1] = 6;
+        m2[1][0] = 2;
+        m2[1][1] = 4;
+        double expected = 2.45;
+        double actual = operacion.distanciaEntreMatrices(m1, m2);
+        assertEquals(expected, actual, 0.1);
+    }
+    
+    @Test
+    public void matrizUnitariaTest() throws Exception{
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        lista[0][0] =new Complejo(3,2);
+        lista[0][1] =new Complejo(9,5);
+        lista[1][0] = new Complejo(5,-6);
+        lista[1][1] = new Complejo(1,4);
+        boolean esperado= false;
+        boolean respuesta = operacion.matrizUnitaria(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+    public void matrizHermitianaTest() throws Exception{
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,8);
+        lista[0][1] =new Complejo(8,1);
+        lista[1][0] = new Complejo(6,-6);
+        lista[1][1] = new Complejo(-6,6);
+        boolean esperado= false;
+        boolean respuesta = operacion.matrizHermitiana(lista) ;
+        assertEquals(respuesta,esperado);
+    }
+    
+    @Test
+    public void tensorMatrizTest(){
+        OperacionesConMatrices operacion = new OperacionesConMatrices();
+        Complejo[][] lista = new Complejo[2][2];
+        Complejo[][] listaDos = new Complejo[2][2];
+        Complejo[][]  esperado = new Complejo[2][2];
+        lista[0][0] =new Complejo(1,0);
+        lista[0][1] =new Complejo(0,1);
+        lista[1][0] = new Complejo(1,0);
+        lista[1][1] = new Complejo(0,1);
+        listaDos[0][0] = new Complejo(0,1);
+        listaDos[0][1] = new Complejo(1,0);
+        listaDos[1][0]= new Complejo(0,1);
+        listaDos[1][1]= new Complejo(1,0);
+        esperado[0][0] = new Complejo(1,1);
+        esperado[0][1] = new Complejo(1,1);
+        esperado[1][0] = new Complejo(1,1);
+        esperado[1][1] = new Complejo(1,1);
+        Complejo[][] respuesta = operacion.productoTensor(lista, listaDos) ;
+        assertEquals(respuesta,esperado);
+    }
+    
 }
